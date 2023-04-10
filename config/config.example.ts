e66b1b2b60250e29715f608e0127f88469363ec6
Kasia <kajzerkatarzyna1@gmail.com>
2023-04-11 00:39:19 +0200

    Added config.example again because of error and moved DB connection data into config

---
+++ config/config.example.ts
@@ -0,0 +1,7 @@
+// export const config = {
+//     dbHost: 'localhost',
+//     dbUser: 'user',
+//     dbPassword: 'password',
+//     dbDatabase: 'database',
+//     corsOrigin: 'http://localhost:3000',
+// };
